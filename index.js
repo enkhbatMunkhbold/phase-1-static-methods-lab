25a4a4b2ea1d9d00a6b43a6bd44688fd726c6c3a
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-02-25 19:34:49 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(str) {
+    const nonCapWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const words = str.split(' ')
+    
+    for(let i = 0; i < words.length; i++) {
+      if(i === 0 || !nonCapWords.includes(words[i])) {
+        words[i] = this.capitalize(words[i])
+      }
+    }
+    return  words.join(' ')
+  }
 }
